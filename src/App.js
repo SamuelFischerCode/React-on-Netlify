b36ecaf222a194cc3c5a28d522a7a8f58fcf1c5f
SamuelFischerCode <65810622+SamuelFischerCode@users.noreply.github.com>
2021-01-08 00:01:00 +0100

    Learned about props and components in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+const Body = props => (
+	<div>
+		<p>
+			{props.contents}
+			{props.num}
+		</p>
+		<p>
+			{props.myObj.foo}
+			{props.myObj.bar}
+			<br></br>
+			{props.myFunc(props.num, 2)}
+			{props.lol}
+		</p>
+		<a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Learn React
+		</a>
+	</div>
+);
+
+const Header = props => (
+	<div className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<Body
+			contents={props.contents}
+			num={props.num}
+			myObj={props.myObj}
+			myFunc={props.myFunc}
+		/>
+	</div>
+);
+
+const App = () => (
+	<div className="App">
+		<Header
+			contents="hi"
+			num={2}
+			myFunc={(a, b) => a + b}
+			myObj={{
+				foo: 'I like',
+				bar: ' React alot',
+			}}
+		/>
+		<Header
+			contents="hi"
+			num={3}
+			myFunc={(a, b) => a + b}
+			myObj={{
+				foo: 'I like',
+				bar: ' React alot',
+			}}
+		/>
+	</div>
+);
 
 export default App;
