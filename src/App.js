f305500ffe6fe2bbf9a737a9037a652a3e26e964
SamuelFischerCode <65810622+SamuelFischerCode@users.noreply.github.com>
2021-01-08 01:21:39 +0100

    Learned the basics of state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
-import { Header } from './components/Header';
+//import './App.css';
+//import { Header } from './components/Header';
+import { Counter } from './components/Counter';
 
 const App = () => (
 	<div className="App">
+		<Counter startCount={0} addSub={1} />
+		<Counter startCount={0} addSub={10} />
+	</div>
+);
+
+export default App;
+
+/*
 		<Header
 			contents="hi"
 			num={2}
@@ -12,16 +21,4 @@ const App = () => (
 				bar: ' React alot',
 			}}
 		/>
-		<Header
-			contents="hi"
-			num={3}
-			myFunc={(a, b) => a + b}
-			myObj={{
-				foo: 'I like',
-				bar: ' React alot',
-			}}
-		/>
-	</div>
-);
-
-export default App;
+		*/
---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+export class Counter extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { count: props.startCount || 0 };
+	}
+
+	handleButtonClick = () => {
+		this.setState({
+			count: this.state.count + this.props.addSub,
+		});
+		//console.log(this.state.count);
+	};
+
+	handleButtonClick2 = () => {
+		this.setState({
+			count: this.state.count - this.props.addSub,
+		});
+		//console.log(this.state.count);
+	};
+
+	render() {
+		return (
+			<div>
+				<div>Count: {this.state.count}</div>
+				<button onClick={this.handleButtonClick}>+{this.props.addSub}</button>
+				<button onClick={this.handleButtonClick2}>-{this.props.addSub}</button>
+			</div>
+		);
+	}
+}
